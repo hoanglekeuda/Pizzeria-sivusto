9564a2dcd457682d12500cfa33b83b514f24d918
Nolami <deata7660@gmail.com>
2024-03-06 09:01:37 +0200

    Update register.js

--- js/register.js
+++ js/register.js
@@ -3,6 +3,13 @@ function register() {
     var password = document.getElementById('password').value
 
     if (validateRegistrationInputs(username, password)) {
-        
+        console.log('käyttäjä rekiströity')
+        window.location.href = 'login.html'
+    } else {
+        console.log('Invalid')
     }
+}
+
+function validateRegistrationInputs() {
+    
 }
