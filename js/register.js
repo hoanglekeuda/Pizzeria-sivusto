1d6885ee74264825cb45141cf8d72915f4d4d4fd
Nolami <deata7660@gmail.com>
2024-03-06 08:57:28 +0200

    ye

---
+++ js/register.js
@@ -0,0 +1,8 @@
+function register() {
+    var username = document.getElementById('username').value
+    var password = document.getElementById('password').value
+
+    if (validateRegistrationInputs(username, password)) {
+        
+    }
+}
